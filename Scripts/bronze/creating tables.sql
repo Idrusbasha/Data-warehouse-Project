27da0d2a5167bda0872f76b651c26d9d428a5f18
Idrusbasha <156606708+Idrusbasha@users.noreply.github.com>
2025-02-28 15:50:24 +0100

    Update creating tables.sql

--- Scripts/bronze/creating tables.sql
+++ Scripts/bronze/creating tables.sql
@@ -18,8 +18,10 @@ cst_marital_status	Nvarchar(50),
 cst_gndr	Nvarchar(50),
 cst_create_date date);
 go
+	
 if OBJECT_ID('bronze_crm_prd_info','U') is not null
 	drop table bronze_crm_prd_info;
+
 go
 create table bronze_crm_prd_info(
 prd_id int,
@@ -31,9 +33,11 @@ prd_start_dt date,
 prd_end_dt date);
 
 go
+	
 if OBJECT_ID('bronze_crm_sales_info','U') is not null
 	drop table bronze_crm_sales_info;
 go
+	
 create table bronze_crm_sales_info(
 sls_ord_num Nvarchar(50),
 sls_prd_key	Nvarchar(100),
@@ -45,24 +49,30 @@ sls_sales int,
 sls_quantity int,
 sls_price int);
 go
+	
 if OBJECT_ID('bronze_erp_cust_az12','U') is not null
 	drop table bronze_erp_cust_az12;
 go
+	
 create table bronze_erp_cust_az12(
 CID Nvarchar(50),
 BDATE date,
 GEN	Nvarchar(50));
 go
+	
 if OBJECT_ID('bronze_erp_loc_a101','U') is not null
 	drop table bronze_erp_loc_a101;
 go
+	
 create table bronze_erp_loc_a101(
 CID Nvarchar(50),
 CNTRY Nvarchar(50));
 go
+	
 if OBJECT_ID('bronze_erp_px_cat_g1v2','U') is not null
 	drop table bronze_erp_px_cat_g1v2;
 go
+	
 create table bronze_erp_px_cat_g1v2(
 ID Nvarchar(50),
 CAT Nvarchar(50),
